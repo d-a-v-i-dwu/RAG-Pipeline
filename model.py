b81b84d42f949a02dfd12c0bebe3b2f2be7cf1b7
d-a-v-i-dwu <davidwdw454@gmail.com>
2025-07-19 21:08:08 +1200

    Commit

--- model.py
+++ model.py
@@ -6,8 +6,8 @@ ollama_model = "llama3.2"
 
 def get_response(prompt):
     # Given a prompt, retrieve relevant chunks from documents and inject it
-    # context = retrieve_similar_chunks(prompt)
-    # final_prompt = f"Using this context: {context}\n Answer this prompt: {prompt}"
+    context = retrieve_similar_chunks(prompt)
+    final_prompt = f"Using this context: {context}\n Answer this prompt: {prompt}"
 
     response = client.generate(model=ollama_model, prompt=prompt)
     return(response.response)
